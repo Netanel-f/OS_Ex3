3bae1b0ec6c4959598aa063589ea9076d0650d0c
jherskow <jjherskow@gmail.com>
2018-05-29 19:21:20 +0300

    Added .cpp for MapReduceFramework Added and possibly finished Makefile Added files to main working folder.

---
+++ MapReduceFramework.cpp
@@ -0,0 +1,50 @@
+
+#include "MapReduceClient.h"
+#include "MapReduceFramework.h"
+
+
+//// ============================   defines and const ==============================================
+
+
+//// ===========================   globals =========================================================
+
+//// ============================   forward declarations for helper funcs ==========================
+
+//// ============================ framework functions ==============================================
+
+
+/**
+ *
+ * @param key
+ * @param value
+ * @param context
+ */
+void emit2 (K2* key, V2* value, void* context){
+
+}
+
+/**
+ *
+ * @param key
+ * @param value
+ * @param context
+ */
+void emit3 (K3* key, V3* value, void* context){
+
+}
+
+/**
+ *
+ * @param client
+ * @param inputVec
+ * @param outputVec
+ * @param multiThreadLevel
+ */
+void runMapReduceFramework(const MapReduceClient& client,
+	const InputVec& inputVec, OutputVec& outputVec,
+	int multiThreadLevel){
+
+}
+
+////===============================  Helper Functions ==============================================
+
