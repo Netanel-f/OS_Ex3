64a71da75fd8a9fb4f148f4c3c75f42fe7beb36b
jherskow <jjerskow@gmail.com>
2018-05-30 17:09:38 +0300

    added new barrier demo.

---
+++ cmake-build-debug/CMakeFiles/OS_Ex3.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cs/usr/jherskow/safe/OS_Ex3/MapReduceFramework.cpp" "/cs/usr/jherskow/safe/OS_Ex3/cmake-build-debug/CMakeFiles/OS_Ex3.dir/MapReduceFramework.cpp.o"
+  "/cs/usr/jherskow/safe/OS_Ex3/main.cpp" "/cs/usr/jherskow/safe/OS_Ex3/cmake-build-debug/CMakeFiles/OS_Ex3.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OS_Ex3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_Ex3.dir/main.cpp.o"
+  "CMakeFiles/OS_Ex3.dir/MapReduceFramework.cpp.o"
+  "OS_Ex3.pdb"
+  "OS_Ex3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS_Ex3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
