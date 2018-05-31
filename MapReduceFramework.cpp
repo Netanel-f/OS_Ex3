e8de51ab113ac3e894151ce981c9c7f818b42007
jherskow <jjerskow@gmail.com>
2018-05-31 10:01:55 +0300

    Started code

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -1,4 +1,5 @@
 
+#include <atomic>
 #include "MapReduceClient.h"
 #include "MapReduceFramework.h"
 
@@ -6,9 +7,33 @@
 //// ============================   defines and const ==============================================
 
 
-//// ===========================   globals =========================================================
+//// ===========================   typedefs & structs =========================================================
+
+//todo struct holding atomic and semaphore
+
+//todo struct holding all inpout and DAST's (?)
+struct data{
+  std::atomic<int>* atomic_counter;
+
+  InputVec* vIn;
+  OutputVec* vOut;
+
+  MapReduceClient& client;
+  InputVec& inputVec;
+  OutputVec& outputVec;
+  int& multiThreadLevel;
+
+};
+
 
 //// ============================   forward declarations for helper funcs ==========================
+
+void threadFlow();
+void mainFlow();
+void initData();
+
+
+bool areEqual(K3 a, K3 b);
 
 //// ============================ framework functions ==============================================
 
@@ -44,7 +69,97 @@ void runMapReduceFramework(const MapReduceClient& client,
 	const InputVec& inputVec, OutputVec& outputVec,
 	int multiThreadLevel){
 
+  // todo init() (make DAST's ?)
+  data stuff;
+  initData(client,inputVec,outputVec,multiThreadLevel, stuff);
+
+
+  // todo call mainFlow()
+
+  // todo finish
+
 }
 
 ////===============================  Helper Functions ==============================================
 
+void initData(const MapReduceClient& client,
+          const InputVec& inputVec, OutputVec& outputVec,
+          int multiThreadLevel, data &stuff){
+  stuff.client = client;
+  stuff.inputVec = inputVec;
+  stuff.outputVec = outputVec;
+  stuff.multiThreadLevel  = multiThreadLevel;
+
+
+}
+
+void threadFlow(){
+
+	//MAP
+
+		// check atomic for new items to be mapped (k1v1)
+
+		// map the items
+
+		// emit the mapped items (k2v2)
+
+	// SORT
+
+		// sort the items
+
+		// emit the sorted items (k2v2) (?)
+
+	// BARRIER
+
+		// get to the barrier
+		// wait
+
+
+	// Reduce
+
+		// wait for new items to be come available
+
+		// make output itemp (k3v3)
+
+
+}
+
+void mainFlow(){
+	//todo same as thread flow, but with additional control segments
+}
+
+void noThreads(){ //todo remove
+
+  //MAP
+
+  // check atomic for new items to be mapped (k1v1)
+
+  // map the items
+
+  // emit the mapped items (k2v2)
+
+  // SORT
+
+  // sort the items
+
+  // emit the sorted items (k2v2) (?)
+
+  // BARRIER
+
+  // get to the barrier
+  // wait
+
+
+  // Reduce
+
+  // wait for new items to be come available
+
+  // make output itemp (k3v3)
+
+}
+
+bool areEqual(K3 &a, K3 &b){
+
+	// neither a<b nor b<a means a==b
+	return !((a<b)||(b<a));
+}
